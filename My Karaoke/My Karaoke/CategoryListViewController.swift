0f9d977dc39f51371e04e7c96680d414f005ff2c
石部達也 <tatsuya_i7@r.recruit.co.jp>
2015-07-18 14:34:34 +0900

    プロジェクト作成

---
+++ My Karaoke/My Karaoke/CategoryListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryListViewController.swift
+//  My Karaoke
+//
+//  Created by 石部達也 on 2015/07/18.
+//  Copyright (c) 2015年 石部達也. All rights reserved.
+//
+
+import UIKit
+
+class CategoryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "My Karaoke"
+    }
+
+}
