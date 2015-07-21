d1ed3d8108c441eff0f0e9148427f4716e598d56
石部達也 <tatsuya_i7@r.recruit.co.jp>
2015-07-22 00:13:13 +0900

    登録API実装

---
+++ My Karaoke/My Karaoke/BaseFetcher.swift
@@ -0,0 +1,13 @@
+//
+//  BaseFetcher.swift
+//  My Karaoke
+//
+//  Created by 石部達也 on 2015/07/21.
+//  Copyright (c) 2015年 石部達也. All rights reserved.
+//
+
+import UIKit
+
+class BaseFetcher: NSObject {
+   
+}
